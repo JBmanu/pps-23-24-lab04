37fcaf746e010b1c617d8680eb402b0dcb17c874
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-03-25 14:43:39 +0100

    feat: green canCreateTeacher

--- src/main/scala/tasks/adts/SchoolModelImpl.scala
+++ src/main/scala/tasks/adts/SchoolModelImpl.scala
@@ -2,12 +2,15 @@ package tasks.adts
 
 import tasks.adts.SchoolModel.SchoolModule
 import u03.Optionals.Optional
+import u03.Sequences.Sequence
 import u03.{Optionals, Sequences}
 
 object SchoolModelImpl extends SchoolModule:
   override type School = this.type
-  override type Teacher = this.type
+  override type Teacher = TeacherImpl
   override type Course = Optional[String]
+
+  case class TeacherImpl(name: String, courses: Sequence[Course])
 
   extension (school: School)
     override def addTeacher(name: String): School = ???
--- src/test/scala/tasks/adts/SchoolModelTest.scala
+++ src/test/scala/tasks/adts/SchoolModelTest.scala
@@ -2,16 +2,28 @@ package tasks.adts
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import tasks.adts.SchoolModelImpl.Course
+//import tasks.adts.SchoolModelImpl.{Course, Teacher, School}
+import tasks.adts.SchoolModelImpl.*
+
+import u03.Sequences.Sequence.*
+
 import u03.Optionals.Optional
 import u03.Optionals.Optional.*
 
 class SchoolModelTest:
+  val course: Course = Just("PPS")
+  val teacher: Teacher = TeacherImpl("Mirko", Cons(course, Nil()))
 
   @Test def canCreateCourse(): Unit =
-    val course: Course = Just("PPS")
     assertEquals(Just("PPS"), course)
-    
-  
+
+  @Test def canCreateTeacher(): Unit =
+    assertEquals("Mirko", teacher.name)
+    assertEquals(Cons(course, Nil()), teacher.courses)
+
+
+
+
+
 
     
