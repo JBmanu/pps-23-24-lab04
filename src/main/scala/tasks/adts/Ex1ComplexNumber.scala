36a328ab509651ac9479dd5530131e88cf6c252c
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-03-25 11:10:30 +0100

    feat: finish es1

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -22,11 +22,25 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Real = Double
+    type Imaginary = Double
+
+    type Complex = ComplexImpl
+    case class ComplexImpl(real: Real, imaginary: Imaginary)
+    def complex(re: Real, im: Imaginary): Complex = ComplexImpl(re, im)
+
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Real = complex.real
+      def im(): Imaginary = complex.imaginary
+      def sum(other: Complex): Complex =
+        ComplexImpl(complex.re() + other.re(), complex.im() + other.im())
+      def subtract(other: Complex): Complex =
+        ComplexImpl(complex.re() - other.re(), complex.im() - other.im())
+      def asString(): String =
+        complex match
+          case ComplexImpl(_, 0) => s"${complex.re()}"
+          case ComplexImpl(0, _) => s"${complex.im()}i"
+          case _ =>
+            val sign = if complex.im() >= 0 then "+" else "-"
+            s"${complex.re()} $sign ${Math.abs(complex.im())}i"
+
