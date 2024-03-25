c094c4a6734357ce1fbde8e46d844c0fb253a62c
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-03-25 15:11:11 +0100

    feat: green addTeacher

--- src/main/scala/tasks/adts/SchoolModelImpl.scala
+++ src/main/scala/tasks/adts/SchoolModelImpl.scala
@@ -1,9 +1,11 @@
 package tasks.adts
 
 import tasks.adts.SchoolModel.SchoolModule
-import u03.Optionals.Optional
 import u03.Sequences.Sequence
-import u03.{Optionals, Sequences}
+import u03.Sequences.Sequence.*
+
+import u03.Optionals.Optional
+import u03.Optionals.Optional.*
 
 object SchoolModelImpl extends SchoolModule:
   override type School = SchoolImpl
@@ -14,12 +16,14 @@ object SchoolModelImpl extends SchoolModule:
   case class SchoolImpl(teachers: Sequence[Teacher], courses: Sequence[Course])
   
   extension (school: School)
-    override def addTeacher(name: String): School = ???
+    override def addTeacher(name: String): School =
+      val teachers = Cons(TeacherImpl(name, Nil()), school.teachers)
+      SchoolImpl(teachers, school.courses)
     override def addCourse(name: String): School = ???
     override def teacherByName(name: String): Optional[Teacher] = ???
     override def courseByName(name: String): Optional[Course] = ???
     override def nameOfTeacher(teacher: Teacher): String = ???
     override def nameOfCourse(teacher: Teacher): String = ???
     override def setTeacherToCourse(teacher: Teacher, course: Course): School = ???
-    override def coursesOfATeacher(teacher: Teacher): Sequences.Sequence[Course] = ???
+    override def coursesOfATeacher(teacher: Teacher): Sequence[Course] = ???
 
--- src/test/scala/tasks/adts/SchoolModelTest.scala
+++ src/test/scala/tasks/adts/SchoolModelTest.scala
@@ -2,9 +2,9 @@ package tasks.adts
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-//import tasks.adts.SchoolModelImpl.{Course, Teacher, School}
 import tasks.adts.SchoolModelImpl.*
 
+import u03.Sequences.Sequence
 import u03.Sequences.Sequence.*
 
 import u03.Optionals.Optional
@@ -13,10 +13,11 @@ import u03.Optionals.Optional.*
 class SchoolModelTest:
   val pps: Course = Just("PPS")
   val pcd: Course = Just("PCD")
-  val courses = Cons(pps, Cons(pcd, Nil()))
+  val courses: Sequence[Course] = Cons(pps, Cons(pcd, Nil()))
   val mirko: Teacher = TeacherImpl("Mirko", Cons(pps, Nil()))
   val alessandro: Teacher = TeacherImpl("Alessandro", Cons(pcd, Nil()))
-  val teachers = Cons(mirko, Cons(alessandro, Nil()))
+  val teachers: Sequence[TeacherImpl] = Cons(mirko, Cons(alessandro, Nil()))
+  val school: School = SchoolImpl(teachers, courses)
 
   @Test def canCreateCourse(): Unit =
     assertEquals(Just("PPS"), pps)
@@ -26,9 +27,15 @@ class SchoolModelTest:
     assertEquals(Cons(pps, Nil()), mirko.courses)
 
   @Test def canCreateSchool(): Unit =
-    val school: School = SchoolImpl(teachers, courses)
     assertEquals(teachers, school.teachers)
     assertEquals(courses, school.courses)
+
+  @Test def addTeacher(): Unit =
+    val schoolAfterAdd = school.addTeacher("Pippo")
+    val newTeachers = Cons(TeacherImpl("Pippo", Nil()), school.teachers)
+    assertEquals(newTeachers, schoolAfterAdd.teachers)
+
+
 
 
 
