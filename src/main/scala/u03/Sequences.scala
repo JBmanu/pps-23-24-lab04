eab2afb0e005a9613dfef5a17c4918e0389c924a
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-03-26 09:52:12 +0100

    refactor: Sequences

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -1,7 +1,7 @@
 package u03
 
 object Sequences: // Essentially, generic linkedlists
-  
+
   enum Sequence[E]:
     case Cons(head: E, tail: Sequence[E])
     case Nil()
@@ -24,21 +24,22 @@ object Sequences: // Essentially, generic linkedlists
     def contains[A](l: Sequence[A])(pred: A => Boolean): Boolean =
       filter(l)(pred) match
         case Cons(h, _) => true
-        case _ => false
+        case _          => false
 
     def remove[A](l: Sequence[A])(pred: A => Boolean): Sequence[A] =
       l match
         case Cons(h, t) if !pred(h) => Cons(h, remove(t)(pred))
-        case _ => Nil()
+        case _                      => Nil()
 
     def substituted[A](l: Sequence[A])(pred: A => Boolean)(value: A): Sequence[A] =
       l match
         case Cons(h, t) if pred(h) => Cons(value, substituted(t)(pred)(value))
-        case Cons(h, t) => Cons(h, substituted(t)(pred)(value))
-        case _ => Nil()
+        case Cons(h, t)            => Cons(h, substituted(t)(pred)(value))
+        case _                     => Nil()
 
-@main def trySequences =
-  import Sequences.* 
+@main
+def trySequences =
+  import Sequences.*
   val l = Sequence.Cons(10, Sequence.Cons(20, Sequence.Cons(30, Sequence.Nil())))
   println(Sequence.sum(l)) // 30
 
