d22f3bbea67be43378b27be18855c5275bdb4373
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-03-26 12:28:29 +0100

    feat: green testEmpty

--- src/main/scala/tasks/adts/Ex3Stack.scala
+++ src/main/scala/tasks/adts/Ex3Stack.scala
@@ -1,6 +1,7 @@
 package tasks.adts
 
 import u03.Sequences.*
+import u03.Sequences.Sequence.*
 import u03.Optionals.*
 
 /*  Exercise 3: 
@@ -22,9 +23,9 @@ object Ex3Stacks:
       def asSequence(): Sequence[A]
   
   object StackImpl extends StackADT:
-    type Stack[A] = Nothing
-    def empty[A]: Stack[A] = ???
+    opaque type Stack[A] = Sequence[A]
+    def empty[A]: Stack[A] = Nil()
     extension [A](stack: Stack[A])
       def push(a: A): Stack[A] = ???
       def pop(a: A): Optional[(A, Stack[A])] = ???
-      def asSequence(): Sequence[A] = ???
+      def asSequence(): Sequence[A] = stack
