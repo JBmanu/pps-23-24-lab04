ecccf123b148014ae96d6d17a8a604ad322b9807
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-03-31 16:51:15 +0200

    fix: green initialValueCounter

---
+++ src/main/scala/tasks/ex7/CounterEx7.scala
@@ -0,0 +1,28 @@
+package tasks.ex7
+
+import u04.monads.States.*
+import u04.monads.{ CounterState, States }
+import u04.monads.CounterStateImpl
+
+
+trait CounterEx7 extends CounterState:
+  extension (value: Counter)
+    def set(): State[Counter, Unit]
+
+object CounterEx7Impl extends CounterEx7:
+  override type Counter = CounterStateImpl.Counter
+
+  override def initialCounter(): Counter = CounterStateImpl.initialCounter()
+
+  override def inc(): State[Counter, Unit] = ???
+
+  override def dec(): State[Counter, Unit] = ???
+
+  override def reset(): State[Counter, Unit] = ???
+
+  override def get(): State[Counter, Int] = ???
+
+  override def nop(): State[Counter, Unit] = ???
+
+  extension (value: Counter)
+    override def set(): State[Counter, Unit] = ???
---
+++ src/test/scala/tasks/ex7/CounterTest.scala
@@ -0,0 +1,14 @@
+package tasks.ex7
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import tasks.ex7.CounterEx7Impl.*
+
+class CounterTest:
+  val initialValue = 0
+
+  @Test def initialValueCounter(): Unit = {
+    assertEquals(initialValue, initialCounter())
+  }
+
+//  @Test def incCounter(): Unit =
